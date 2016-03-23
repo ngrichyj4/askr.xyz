469aa694d709c6aaabc33c1c97e2c42c57d25ada
Alexander Bird <alexkbird@gmail.com>
2016-03-22 23:38:28 -0600

    create poll (question and voting style only)

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -3,12 +3,43 @@ class PollsController < ApplicationController
     @poll = Poll.new
   end
 
-  def router
-    create
+  def create
+    poll = Poll.create params_for_create
+    redirect_to polls_url
   end
 
-  def create
-    @poll = Poll.create slug: params[:slug]
-    render template: 'polls/create'
+  def router
+    if poll_already_exists 
+      if user_already_voted
+        # TODO: view results
+      else
+        vote
+      end
+    else
+      new
+    end
+  end
+
+  def new
+    @poll = Poll.new slug: params[:slug]
+    render template: 'polls/new'
+  end
+
+  def vote
+    @poll = Poll.find_by_slug params[:slug]
+    render template: 'polls/vote'
+  end
+
+private
+  def poll_already_exists
+    Poll.where(slug: params[:slug]).any?
+  end
+
+  def user_already_voted
+    false # TODO - do something session-ey for this?
+  end
+
+  def params_for_create
+    params.require(:poll).permit(:slug, :question, :voting_style)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "polls#landing"
-  get '/:slug', to: 'polls#router'
+  get '/:slug', to: 'polls#router', as: :polls
+  post '/:slug', to: 'polls#create'
 end
