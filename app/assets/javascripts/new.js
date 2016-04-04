8abc341f2cc2d94a2ae238d5ccead736abba18a9
Stas <stas.tserkovny@gmail.com>
2016-04-04 12:33:48 -0600

    Added option adding/removing
    Added the ability to add and remove options from the vote page

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,20 +1,85 @@
 var Askr = Askr || {};
 
 Askr.NewPage = (function () {
+    // Delete element function
     function DeleteElementClick(buttonId) {
-        // implement this here
+        $( '#option_row_'+buttonId ).remove();
         console.log("Deleting element " + buttonId);
     }
 
-    function Init () {
+    var elementCounter = 10;
+    // Add option button
+    function AddOptionClick() {
+        // implement addint option here
+        console.log("Adding "+ elementCounter + "th option.");
+        $( '#add_option_li' ).before(
+            "<li id='option_row_" + elementCounter + "'></li>"
+        );
+        $( '<span></span>')
+            .attr("id", "option_span_"+elementCounter)
+            .attr("class", "input-group")
+            .appendTo($( '#option_row_'+elementCounter ));
+
+        $( '<input type="text"></input>')
+            .attr("name", "poll[options_attributes][" + elementCounter + "][text]")
+            .attr("class", "form-control")
+            .appendTo($( '#option_span_'+elementCounter ));
+
+        $( '<span type="text"></span>')
+            .attr("name", "poll[option_attributes][" + elementCounter + "][text]")
+            .attr("id", "option_button_span_"+elementCounter)
+            .attr("class", "input-group-btn")
+            .appendTo($( '#option_span_'+elementCounter ));
+
+        $( '<button id="' + elementCounter + '" class ="btn btn-danger deleteButton">' +
+                '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+            '</button>').appendTo( '#option_button_span_'+elementCounter );
+
+        InitDeletes();
+
+        elementCounter ++;
+    }
+
+    // validate form input
+    function ValidateQuestionForm() {
+        // implement form validation
+        console.log("Validating input");
+        //if ($( '#poll_question' ).text() == '') {
+        //    poll question text is empty
+        //    console.log("Missing poll question!");
+        //    return false;
+        //}
+        return true;
+    }
+
+    function InitDeletes() {
+        // delete option function binding
         $( ".deleteButton" ).each(function () {
             $( this ).click(function(e) {
                 e.preventDefault();
                 DeleteElementClick(this.id);
                 return false;
             });
-            console.log("Did something with buttons" + this);
         });
+    }
+    function Init () {
+        InitDeletes();
+
+        // add option function binding
+        $( "#addOptionBtn" ).click(function(e) {
+            e.preventDefault();
+            AddOptionClick();
+            return false;
+        });
+
+        // submit form validation function binding
+        $( 'input[name=commit]' ).click(function(e){
+            if (!ValidateQuestionForm()) {
+                e.preventDefault();
+                return false;
+            }
+        });
+
     }
 
     return {
